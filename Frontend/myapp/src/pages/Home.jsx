73d5ce19a5b867e9b169e2204123e2409619f550
Priyanshu Bindal <bindalpriyanshu6@gmail.com>
2024-11-27 14:02:18 +0530

    home changes button added

--- Frontend/myapp/src/pages/Home.jsx
+++ Frontend/myapp/src/pages/Home.jsx
@@ -1,10 +1,23 @@
 import React from "react";
 import { useAppContext } from "../configs/AppContext";
 import './Home.css';  
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
     const context = useAppContext();
+    const navigate = useNavigate();
+
+    const handleLogin = (e) => {
+        e.preventDefault();
+
+        navigate('/login');
+    }
+
+    const handleRegister = (e) => {
+        e.preventDefault();
+
+        navigate('/register');
+    }
 
     return (
         <div className="home-container">
@@ -13,8 +26,11 @@ const Home = () => {
                 <p>Hello, <span>{context.user ? context.user.name : 'Guest'}</span>!</p>
                 <p>We are so glad to have you here. Feel free to explore the website!</p>
             </div>
-            <button type="button">
+            <button type="button" onClick={handleLogin}>
                 Login
+            </button>
+            <button type="button" onClick={handleRegister}>
+                Register
             </button>
         </div>
     );
